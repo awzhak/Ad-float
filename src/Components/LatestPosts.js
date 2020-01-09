fd4ac33aa29040035a7a6dff8c1597ef3b9b37ae
micrm1 <kd1229638@st.kobedenshi.ac.jp>
2020-01-09 11:02:06 +0900

    homeの最新の投稿一覧表示

--- src/Components/LatestPosts.js
+++ src/Components/LatestPosts.js
@@ -1,25 +1,50 @@
 import React, { useState } from 'react';
 import { Card, Button } from 'react-bootstrap'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    width: "14rem",
+    marginRight: 3,
+    marginBottom: 15
+  },
+  cardgrid: {
+    textAlign: "center",
+    margin: 20
+  }
+}));
 
 function LatestPosts() {
-    return (
-      <>
+  const classes = useStyles();
+
+  const colors = ['Red','Yellow','Green','Blue','Violet', 'Red','Yellow','Green','Blue','Violet', 'Red','Yellow','Green','Blue','Violet'];
+  
+  const latestpost = colors.map((color,index) =>
+    <div class="grid-item">
+      <Card className={classes.card}>
+        <Card.Img variant="top" src="aaa.svg" />
+        <Card.Body>
+          <Card.Title>{color}</Card.Title>
+          <Card.Text>
+            the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+
+  return (
+    <div className={classes.cardgrid}>
+      <Typography variant="h3" gutterBottom color='primary'>
+        最新の投稿
+      </Typography>
       <div class="grid js-masonry" data-masonry-options='{ "itemSelector": ".grid-item", "columnWidth": 15 }'>
-        <div class="grid-item">
-          <Card style={{ width: '12rem' }}>
-            <Card.Img variant="top" src="aaa.svg" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </div>
+        {latestpost}
       </div>
-      </>
-    );
+    </div>
+  );
   }
   
   export default LatestPosts;
