cedfed2ee7dab333f4f09400934906e1485a89d6
kd-ryouji <kd1234055@st.kobedenshi.ac.jp>
2020-01-21 09:27:14 +0900

    データベース接続前

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Original_Navbar from './Components/Original_Navbar';
-import ArtDetails from './Components/ArtDetails';
+import PersonalProfileEdit from './Components/PersonalProfileEdit';
 
 
 function App() {
   return (
     <div className="App">
       <Original_Navbar />
-      <ArtDetails />
+      <PersonalProfileEdit />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,26 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from "react-redux";
 import * as serviceWorker from './serviceWorker';
-import firebase from 'firebase/app'
 import 'firebase/auth'
 
 //BootStrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyARvK9zX7nleYdflDo7Pn4uK6xovk4vyiw",
-    authDomain: "adfloat-6b34d.firebaseapp.com",
-    databaseURL: "https://adfloat-6b34d.firebaseio.com",
-    projectId: "adfloat-6b34d",
-    storageBucket: "adfloat-6b34d.appspot.com",
-    messagingSenderId: "288318652594",
-    appId: "1:288318652594:web:bdf0e5ac1a60740b1809ab",
-    measurementId: "G-Y6FX1ZLKWC"
-};
-firebase.initializeApp(firebaseConfig)
+
 
 
 ReactDOM.render(
